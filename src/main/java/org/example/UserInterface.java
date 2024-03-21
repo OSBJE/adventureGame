9cda92577962b4840c1afa0f7705c5ad40afb703
OSBJE <osbj0001rstud.kea.dk>
2024-03-21 10:40:41 +0100

    OSBJE Commit10 - del 3 af opgaven

--- src/main/java/org/example/UserInterface.java
+++ src/main/java/org/example/UserInterface.java
@@ -6,20 +6,16 @@ import java.util.Objects;
 import java.util.Scanner;
 
 public class UserInterface {
+
+    ///********** Generate instance of Adventure Controler connect to game *************************///
     Scanner input = new Scanner(System.in);
-
-    // instance / laver et Game object
-    // laver en nyt game
-
     AdventureController spil = new AdventureController();
+
+    // --- Helper to know previousRoom --- //
     private String previousRoom;
 
-    //MMH Vi kunne definere constructor og køre startGame herinde. Så skal metodekald af startGame slettes fra Main
-    public UserInterface() {
-        welcome();
-        startAndPlayGame();
-    }
-
+    // --- Main caller den her metode for at starte game --- //
+    ///********** Game master this keeps game in a loop and calls methods *************************///
     public void startAndPlayGame() {
 
         final String SENTINEL = "exit";
@@ -104,6 +100,13 @@ public class UserInterface {
 
     }
 
+    public UserInterface() {
+        welcome();
+        startAndPlayGame();
+    }
+
+
+    ///********** Helper metods and print out practical information  *******************************///
     public void welcome() {
         System.out.println(" ");
         System.out.println("Welcome to the AdventureGame - MULTIVERSE");
@@ -112,7 +115,23 @@ public class UserInterface {
         System.out.println("You start in room1 where you find yourself standing on a \nbeautiful green hill with a marvelous view...\n");
     }
 
-    //denne metode tager input fra player, laver til små bogstaver, deler op på ord i nyt Array, så disse kan bruges videre
+    // --- Helper metod to get general practical information --- //
+    public void helpCommands() { //bare for at samle sysout. Kan fjernes, hvis helt dumt.
+        System.out.println("----------------------------------------------------------------");
+        System.out.println("Help functions:");
+        System.out.println(" ");
+        System.out.println("Type direction you want to move.");
+        System.out.println("Type take and name of item, if you want to take an item from a room. ");
+        System.out.println("Type drop and name of item, if you want to drop an item in a room. ");
+        System.out.println("Type look to get a description of the current room. ");
+        System.out.println("Type eat and name of food you want to eat. ");
+        System.out.println("Type equip and name of weapon you want to use. ");
+        System.out.println("Type attack to perform an attack on the enemy closest to you. This command can also be followed by the enemy's name.");
+        System.out.println("Type exit to terminate the program.");
+        System.out.println("---------------------------------------------------------------- ");
+    }
+
+    // --- Helper metode to manipulate player inputs --- //
     public String[] playerInputManipulation(String playerInput) {
         String playerInput2 = playerInput.toLowerCase();
         String[] output = playerInput2.split(" ");
@@ -127,6 +146,85 @@ public class UserInterface {
         return new String[]{firstWord, secondWord, playerInput3};
 
     }
+
+    // --- method to get player health --- //
+    public void showPlayerHealthUI() { ///Sysouts af nuværende player health. Thresholds ligger på højere eller lige med 50 & 30 med sidste else.
+        if (spil.getHealthPlayer() >= 50) {
+            System.out.println("Your health is currently at " + spil.getHealthPlayer() + "." + " You are in good health and feel super duper.");
+        } else if (spil.getHealthPlayer() < 30) {
+            System.out.println("Your health is currently at " + spil.getHealthPlayer() + "." + " You feel dizzy and might cross into the afterlife anytime soon.");
+        } else {
+            System.out.println("Your health is currently at " + spil.getHealthPlayer() + "." + " Your health is getting low now. You better avoid fighting.");
+        }
+    }
+
+    // --- if dead display information --- //
+    public void playerisDeadMessageUI() {
+        if (spil.getHealthPlayer() <=0) {
+            System.out.println("\nYour health is currently at "+spil.getHealthPlayer()+".");
+            System.out.println("You have sustained critical injury and are now susceptible the enemies lurking nearby.");
+            System.out.println("You succumb to your wounds and perish.");
+        }
+    }
+
+    // --- Helper metod to playerisDeaMessageUI --- //
+    public boolean isPlayerDead() {
+        if(spil.getHealthPlayer() <= 0) {
+            return true;
+        }
+        return false;
+    }
+
+    ///********** Player Navigation methods   ****************************************************///
+
+    public void playerMovement(String userChoice) {
+        spil.movePlayer(userChoice);
+        addInformation();
+    }
+
+    // --- Helper method to playerMovement to get information in room --- //
+    public void addInformation() {
+        //MMH her vil jeg gerne have den til at udskrive noget om hvor spilleren befinder sig, og en melding, hvis vedkomende ikke kan gå den vej
+        //Til dette bruger jeg lokal variable previousRoom og currentRoom
+        String currentRoom = spil.getCurrentRoom();//MMH variabel så kan tjekke om spiller har flyttet sig
+        if (previousRoom.equals(currentRoom)) {
+            System.out.println("Unfortunately you cannot go in this direction");
+            System.out.println("You are still located in " + spil.getCurrentRoom());
+        } else {
+            System.out.println("You are now located in " + spil.getCurrentRoom());
+            System.out.println(spil.getCurrentRoomDescription());
+            System.out.println(" ");
+            displayItemsInRoom();
+            displayEnemyInRoom();
+            System.out.println(" ");
+        }
+        previousRoom = currentRoom;
+    }
+
+    public void displayItemsInRoom() { //Metode til at displaye items i room. Items bliver foreløbigt sat i buildMap().
+        if (!spil.getitemsArrayList().isEmpty()) { //Hvis arraylisten IKKE er tom
+            System.out.println("You spot items that may be of use to you in this room: ");
+            for (Item item : spil.getitemsArrayList()) { //For each loop itererer gennem vores itemsArrayList for at finde Items, der er forbundet med det enkelte rum.
+                System.out.println(item.getItem());
+            }
+        } else {
+            System.out.println("You see nothing of use to you in this room.");
+        }
+    }
+
+    public void displayEnemyInRoom() { //Metode til at displaye items i room. Items bliver foreløbigt sat i buildMap().
+        if (!spil.getEnemyArrayList().isEmpty()) { //Hvis arraylisten IKKE er tom
+            System.out.println("You spot enemies in this room: ");
+            for (Enemy enemy : spil.getEnemyArrayList()) { //For each loop itererer gennem vores itemsArrayList for at finde Items, der er forbundet med det enkelte rum.
+                System.out.println(enemy.toString());
+            }
+        } else {
+            System.out.println("You see no enemies in this room.");
+        }
+    }
+
+    /// ************** Item handling between room and player inventory  *************************** ///
+
 
     public void takeItemChoice(String chosenItem) {
         String result = spil.takeItemMethod(chosenItem);
@@ -147,11 +245,50 @@ public class UserInterface {
 
     }
 
-    public void playerMovement(String userChoice) {
-        spil.movePlayer(userChoice);
-        addInformation();
+    // --- Method to eat Item --- //
+    public void eatWithCheckofEatability(String chosenitem) {
+        if (spil.playerEatsFood(chosenitem).equals("foodOkay")) {
+            System.out.println("Bon appetite");
+        } else if (spil.playerEatsFood(chosenitem).equals("notFood")) {
+            System.out.println("Unfortunately you cannot eat this item.");
+        } else if (spil.playerEatsFood(chosenitem).equals("foodNotAvailable")) {
+            System.out.println("You do not have this available.");
+        }
     }
 
+    // --- Getter metod to display playerInventory --- //
+    public void playerInventory() {
+        if (spil.getPlayerInventory().length() > 0) {
+            String playerInventory = spil.getPlayerInventory();
+            System.out.println(playerInventory);
+        } else {
+            System.out.println("Your inventory is empty.");
+        }
+
+    }
+
+    // --- Methods to equip weapon from inventory --- //
+    public void equipWeapon(String input) {
+        if (spil.setEquipedWeapon(input).equals("notWeapon")) {
+            System.out.println("This is not an available weapon in your bag...find something else.");
+            System.out.println(" ");
+        } else {
+            String equipedItem = spil.getPlayerEquiped();
+            System.out.println("You have the following weapon equipped: " + equipedItem);
+            System.out.println(" ");
+        }
+    }
+
+    // --- Getter metod to display equipedWeapon --- //
+    public void playerEquiped() {
+        System.out.println("\n you have the following weapon equiped: ");
+        String equiped = spil.getPlayerEquiped();
+        System.out.println(equiped);
+    }
+
+    /// ************** Attack and combat functions and methods  ************************************* ///
+
+    // --- Method to attack random enemy --- //
     public void attackFunction() { //Til attacks ud i luften, uden enemy parameter. Implementer sammen med attack branch i switch
         if (spil.doIHaveWeaponEquipped()) {
             if (spil.getRemainingShots() > 0) {
@@ -200,6 +337,7 @@ public class UserInterface {
         }
     }
 
+    // --- Helper method to attackFunction --- //
     public Enemy supportAttackFunctionTarget(String input) { // Vi leder efter Enemy objekter baseret på stringinput.
         ArrayList<Enemy> enemies = spil.getEnemyArrayList();
         for (Enemy enemy : enemies) {
@@ -211,123 +349,6 @@ public class UserInterface {
     }
 
 
-    public void addInformation() {
-        //MMH her vil jeg gerne have den til at udskrive noget om hvor spilleren befinder sig, og en melding, hvis vedkomende ikke kan gå den vej
-        //Til dette bruger jeg lokal variable previousRoom og currentRoom
-        String currentRoom = spil.getCurrentRoom();//MMH variabel så kan tjekke om spiller har flyttet sig
-        if (previousRoom.equals(currentRoom)) {
-            System.out.println("Unfortunately you cannot go in this direction");
-            System.out.println("You are still located in " + spil.getCurrentRoom());
-        } else {
-            System.out.println("You are now located in " + spil.getCurrentRoom());
-            System.out.println(spil.getCurrentRoomDescription());
-            System.out.println(" ");
-            displayItemsInRoom();
-            displayEnemyInRoom();
-            System.out.println(" ");
-        }
-        previousRoom = currentRoom;
-    }
-
-    public void displayItemsInRoom() { //Metode til at displaye items i room. Items bliver foreløbigt sat i buildMap().
-        if (!spil.getitemsArrayList().isEmpty()) { //Hvis arraylisten IKKE er tom
-            System.out.println("You spot items that may be of use to you in this room: ");
-            for (Item item : spil.getitemsArrayList()) { //For each loop itererer gennem vores itemsArrayList for at finde Items, der er forbundet med det enkelte rum.
-                System.out.println(item.getItem());
-            }
-        } else {
-            System.out.println("You see nothing of use to you in this room.");
-        }
-    }
-
-    public void displayEnemyInRoom() { //Metode til at displaye items i room. Items bliver foreløbigt sat i buildMap().
-        if (!spil.getEnemyArrayList().isEmpty()) { //Hvis arraylisten IKKE er tom
-            System.out.println("You spot enemies in this room: ");
-            for (Enemy enemy : spil.getEnemyArrayList()) { //For each loop itererer gennem vores itemsArrayList for at finde Items, der er forbundet med det enkelte rum.
-                System.out.println(enemy.toString());
-            }
-        } else {
-            System.out.println("You see no enemies in this room.");
-        }
-    }
-
-    public void playerInventory() {
-        if (spil.getPlayerInventory().length() > 0) {
-            String playerInventory = spil.getPlayerInventory();
-            System.out.println(playerInventory);
-        } else {
-            System.out.println("Your inventory is empty.");
-        }
-
-    }
-
-    //-----Weapon Management--------------------------------------------------------------------------
-    public void playerEquiped() {
-        System.out.println("\n you have the following weapon equiped: ");
-        String equiped = spil.getPlayerEquiped();
-        System.out.println(equiped);
-    }
-
-    public void equipWeapon(String input) {
-        if (spil.setEquipedWeapon(input).equals("notWeapon")) {
-            System.out.println("This is not an available weapon in your bag...find something else.");
-            System.out.println(" ");
-        } else {
-            String equipedItem = spil.getPlayerEquiped();
-            System.out.println("You have the following weapon equipped: " + equipedItem);
-            System.out.println(" ");
-        }
-    }
-
-
-    public void helpCommands() { //bare for at samle sysout. Kan fjernes, hvis helt dumt.
-        System.out.println("----------------------------------------------------------------");
-        System.out.println("Help functions:");
-        System.out.println(" ");
-        System.out.println("Type direction you want to move.");
-        System.out.println("Type take and name of item, if you want to take an item from a room. ");
-        System.out.println("Type drop and name of item, if you want to drop an item in a room. ");
-        System.out.println("Type look to get a description of the current room. ");
-        System.out.println("Type eat and name of food you want to eat. ");
-        System.out.println("Type equip and name of weapon you want to use. ");
-        System.out.println("Type attack to perform an attack on the enemy closest to you. This command can also be followed by the enemy's name.");
-        System.out.println("Type exit to terminate the program.");
-        System.out.println("---------------------------------------------------------------- ");
-    }
-
-    public void eatWithCheckofEatability(String chosenitem) {
-        if (spil.playerEatsFood(chosenitem).equals("foodOkay")) {
-            System.out.println("Bon appetite");
-        } else if (spil.playerEatsFood(chosenitem).equals("notFood")) {
-            System.out.println("Unfortunately you cannot eat this item.");
-        } else if (spil.playerEatsFood(chosenitem).equals("foodNotAvailable")) {
-            System.out.println("You do not have this available.");
-        }
-    }
-
-
-    public void showPlayerHealthUI() { ///Sysouts af nuværende player health. Thresholds ligger på højere eller lige med 50 & 30 med sidste else.
-        if (spil.getHealthPlayer() >= 50) {
-            System.out.println("Your health is currently at " + spil.getHealthPlayer() + "." + " You are in good health and feel super duper.");
-        } else if (spil.getHealthPlayer() < 30) {
-            System.out.println("Your health is currently at " + spil.getHealthPlayer() + "." + " You feel dizzy and might cross into the afterlife anytime soon.");
-        } else {
-            System.out.println("Your health is currently at " + spil.getHealthPlayer() + "." + " Your health is getting low now. You better avoid fighting.");
-        }
-    }
-    public void playerisDeadMessageUI() {
-        if (spil.getHealthPlayer() <=0) {
-            System.out.println("\nYour health is currently at "+spil.getHealthPlayer()+".");
-            System.out.println("You have sustained critical injury and are now susceptible the enemies lurking nearby.");
-            System.out.println("You succumb to your wounds and perish.");
-        }
-    }
-    public boolean isPlayerDead() {
-        if(spil.getHealthPlayer() <= 0) {
-            return true;
-        }
-        return false;
-    }
 }
 
 
